99810668a19441ee848e93043d73d779f10c17b1
Daniel Vidales <daniel.vidales@dexcom.com>
2023-06-19 05:34:51 -0700

    User Authentication dependency for findNavController access within Fragment

--- app/src/main/java/com/dexcom/sdk/locationreminders/login/LoginFragment.kt
+++ app/src/main/java/com/dexcom/sdk/locationreminders/login/LoginFragment.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.dexcom.sdk.locationreminders.R
 import com.dexcom.sdk.locationreminders.databinding.FragmentLoginBinding
 import com.firebase.ui.auth.AuthUI
@@ -15,6 +14,7 @@ import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
