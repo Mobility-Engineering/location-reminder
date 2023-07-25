7fe7079b62ae435d74b0cbe7ebab50ed152442e9
Daniel Vidales <daniel.vidales@dexcom.com>
2023-07-25 05:42:15 -0700

    observerReminders() method added to ReminderDao

--- app/src/main/java/com/dexcom/sdk/locationreminders/database/ReminderDatabase.kt
+++ app/src/main/java/com/dexcom/sdk/locationreminders/database/ReminderDatabase.kt
@@ -12,58 +12,55 @@ data class Reminder(val id: Long, val codename: String, val closeApproachDate: S
                     val isPotentiallyHazardous: Boolean) : Parcelable
                     */
 
-@Entity(tableName = "reminder_table")
-data class DatabaseReminder constructor(
-    @PrimaryKey(autoGenerate = true)
-    val id: Long,
-    @ColumnInfo(name= "name")
-    val name:String,
-    @ColumnInfo(name = "latitude")
-    val latitude: Double,
-    @ColumnInfo(name = "longitude")
-    val longitude: Double,
-    @ColumnInfo(name = "title")
-    val title: String,
-    @ColumnInfo(name = "description")
-    val description: String,
-)
 
-fun List<DatabaseReminder>.asDomainModel(): List<Reminder> {
+
+fun List<Reminder>.asDomainModel(): List<Reminder> {
     return map {
         Reminder(
             id = it.id,
-            name =it.name,
+            name = it.name,
             title = it.title,
             description = it.description,
-            latitude =  it.latitude,
-            longitude  = it.longitude
-        
+            latitude = it.latitude,
+            longitude = it.longitude
+
         )
     }
 }
 
 @Dao
-interface ReminderDao {
+interface RemindersDao {
+
+    /**
+     * Observes list of tasks.
+     *
+     * @return all tasks.
+     */
     @Query("SELECT * from reminder_table")
-    fun getReminders(): List<DatabaseReminder>
+    fun observeReminders(): LiveData<List<Reminder>>
+
+    /*
+    @Query("SELECT * from reminder_table")
+    suspend fun getReminders(): List<Reminder>
+
+     */
 
     //As mentioned in the comments, remove suspend. When a method returns an observable, there is no reason to make it suspend
     // since it just returns and object, does not run any query until it is observed.
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAll(vararg reminder: DatabaseReminder)
+    fun insertAll(vararg reminder: Reminder)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(reminder: DatabaseReminder)
+    fun insertReminder(reminder: Reminder)
 
     @Query("DELETE FROM reminder_table")
     fun clear()
-
 }
 
-@Database(entities = [DatabaseReminder::class], version = 2)
+@Database(entities = [Reminder::class], version = 2)
 abstract class ReminderDatabase : RoomDatabase() {
-    abstract val reminderDao: ReminderDao
+    abstract fun remindersDao(): RemindersDao
 
     companion object {
         /* The value of a volatile variable will never be cached, and all writes and
